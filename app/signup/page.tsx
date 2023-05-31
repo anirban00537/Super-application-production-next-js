1863dd4e3b3ae099ebfb3db4d0e681ba0b492ea3
anirban00537 <anirban00537@gmail.com>
2023-05-31 13:51:48 +0600

    added login

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -29,7 +29,7 @@ const page = () => {
               </svg>
             </a>
             <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-              Welcome to Feedlio 🦑
+              Welcome to Squid 🦑
             </h2>
             <p className="mt-4 leading-relaxed text-white/90">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi
@@ -69,7 +69,35 @@ const page = () => {
               </p>
             </div>
             <form action="#" className="mt-8 grid grid-cols-6 gap-6">
-              <div className="col-span-12">
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  name="first_name"
+                  className="w-full px-3 py-2 border rounded"
+                />
+              </div>
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="LastName"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  id="LastName"
+                  name="last_name"
+                  className="w-full px-3 py-2 border rounded"
+                />
+              </div>
+              <div className="col-span-6">
                 <label
                   htmlFor="Email"
                   className="block text-sm font-medium text-gray-700"
@@ -81,10 +109,9 @@ const page = () => {
                   id="Email"
                   name="email"
                   className="w-full px-3 py-2 border rounded"
-                  style={{ width: "100%" }}
                 />
               </div>
-              <div className="col-span-12">
+              <div className="col-span-6 sm:col-span-3">
                 <label
                   htmlFor="Password"
                   className="block text-sm font-medium text-gray-700"
@@ -96,18 +123,59 @@ const page = () => {
                   id="Password"
                   name="password"
                   className="w-full px-3 py-2 border rounded"
-                  style={{ width: "100%" }}
+                />
+              </div>
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="PasswordConfirmation"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password Confirmation
+                </label>
+                <input
+                  type="password"
+                  id="PasswordConfirmation"
+                  name="password_confirmation"
+                  className="w-full px-3 py-2 border rounded"
                 />
               </div>
               <div className="col-span-6">
+                <label htmlFor="MarketingAccept" className="flex gap-4">
+                  <input
+                    type="checkbox"
+                    id="MarketingAccept"
+                    name="marketing_accept"
+                    className="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm"
+                  />
+                  <span className="text-sm text-gray-700">
+                    I want to receive emails about events, product updates and
+                    company announcements.
+                  </span>
+                </label>
+              </div>
+              <div className="col-span-6">
+                <p className="text-sm text-gray-500">
+                  By creating an account, you agree to our
+                  <a href="#" className="text-gray-700 underline">
+                    terms and conditions
+                  </a>
+                  and
+                  <a href="#" className="text-gray-700 underline">
+                    privacy policy
+                  </a>
+                  .
+                </p>
+              </div>
+              <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
                 <button className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
-                  Login
+                  Create an account
                 </button>
-                <p className="mt-4 text-sm text-gray-500">
+                <p className="mt-4 text-sm text-gray-500 sm:mt-0">
                   Already have an account?
                   <a href="#" className="text-gray-700 underline">
-                    Signup
+                    Log in
                   </a>
+                  .
                 </p>
               </div>
             </form>
