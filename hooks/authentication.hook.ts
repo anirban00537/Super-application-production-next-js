54bf796766db1d14d0ae2e3e41a40f289ac6b6db
anirban00537 <anirban00537@gmail.com>
2023-06-09 19:39:09 +0600

    working

---
+++ hooks/authentication.hook.ts
@@ -0,0 +1,132 @@
+//@ts-ignore
+import Cookies from "js-cookie";
+
+import { processResponse } from "@/utils/functions";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { verifyEmailApi ,ResetPasswordApi,forgotPasswordApi,loginApi,signupApi} from "@/service/authentication";
+import { setUser } from "@/state/reducer/user";
+import { GetUserProfile } from "@/service/user";
+
+export const useSignin = () => {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const { mutateAsync, isLoading } = useMutation((data: any) => {
+    return loginApi(data.email, data.password);
+  });
+
+  const handleLogin = async (email: string, password: string) => {
+    try {
+      const response = await mutateAsync({ email, password });
+      processResponse(response);
+      if (response.success) {
+        router.push("/dashboard");
+        Cookies.set("token", response?.data?.accessToken);
+      }
+    } catch (error) {
+      processResponse(error);
+    }
+  };
+
+  return { register, handleSubmit, errors, handleLogin, isLoading };
+};
+export const useSignup = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const { mutateAsync, isLoading } = useMutation((data: any) => {
+    return signupApi(
+      data.email,
+      data.password,
+      data.first_name,
+      data.last_name,
+      data.nick_name
+    );
+  });
+
+  const handleSignup = async (
+    email: string,
+    password: string,
+    first_name: string,
+    last_name: string,
+    nick_name: string
+  ) => {
+    try {
+      const data = await mutateAsync({
+        email,
+        password,
+        first_name,
+        last_name,
+        nick_name,
+      });
+      processResponse(data);
+      router.push("/verify-email");
+    } catch (error) {
+      processResponse(error);
+    }
+  };
+
+  return { register, handleSubmit, errors, handleSignup, isLoading };
+};
+export const useVerifyEmail = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const { mutateAsync, isLoading } = useMutation((data: any) => {
+    return verifyEmailApi(data.email, data.code);
+  });
+
+  const handleVerifyCode = async (email: string, code: string) => {
+    try {
+      const data = await mutateAsync({ email, code });
+      processResponse(data);
+      if (data.success) {
+        router.push("/login");
+      }
+    } catch (error) {
+      processResponse(error);
+    }
+  };
+
+  return { register, handleSubmit, errors, handleVerifyCode, isLoading };
+};
+export const useCheckAuthState = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const { data: userProfile, isLoading: isProfileLoading } = useQuery({
+    retry: 0,
+    queryKey: ["user"],
+    queryFn: () => GetUserProfile(),
+    onSuccess: (data) => {
+      if (data.success === true) {
+        dispatch(setUser(data.data));
+      }
+    },
+    onError: () => {
+      router.push("/login");
+    },
+    enabled: !!Cookies.get("token"),
+  });
+
+  return {
+    isLoading: isProfileLoading,
+    user: userProfile?.data,
+  };
+};
--- layout/index.tsx
+++ layout/index.tsx
@@ -2,41 +2,16 @@
 
 import { Database } from "@/lib/database.types";
 import { useAppDispatch, useAppSelector } from "@/state/hooks";
-import { login } from "@/state/reducer/user";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import LoadingComponent from "@/sections/Loading";
+import { useCheckAuthState } from "@/hooks/authentication.hook";
 
 const Layout = ({ children }: any) => {
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
-  const dispatch = useAppDispatch();
-  const { user, isLoggedIn } = useAppSelector((state) => state.user);
-
-  const supabase = createClientComponentClient<Database>();
-  const fetchUserCredentials = async () => {
-    try {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-
-      if (user) {
-        dispatch(login(user));
-        setLoading(false);
-      } else {
-        await router.push("/");
-        await setLoading(false);
-      }
-    } catch (error: any) {
-      toast.error(error);
-    } finally {
-    }
-  };
-  useEffect(() => {
-    fetchUserCredentials();
-  }, []);
+  const { user, isLoggedIn, loading } = useAppSelector((state) => state.user);
+  useCheckAuthState();
   if (loading) return <LoadingComponent />;
 
   return <div>{children}</div>;
